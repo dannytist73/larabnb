a61ad4533356b0eabb3d1a9f70e5dd2281ac802d
dannytist <75296514+dannytist@users.noreply.github.com>
2024-06-20 14:45:30 +0800

    started the project; first commit

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        Index
+    </div>
+    <div>The message is {{ message }}</div>
+</template>
+
+<script setup>
+import {Link} from '@inertiajs/vue3'
+defineProps({
+    message: String
+})
+</script>
---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        Show
+    </div>
+</template>
+
+<script setup>
+import {Link} from '@inertiajs/vue3'
+
+</script>
