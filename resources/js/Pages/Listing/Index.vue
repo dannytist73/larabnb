e23297a0a68a272f85d8b25463e6538213fef8aa
dannytist <75296514+dannytist@users.noreply.github.com>
2024-06-21 17:14:46 +0800

    learning useform

---
+++ resources/js/Pages/Listing/Index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div v-for="listing in listings" :key="listing.id">
+        <Link :href="`/listing/${listing.id}`">
+            <ListingAddress :listing="listing"/>
+        </Link>
+    </div>
+</template>
+
+<script setup lang="ts">
+import ListingAddress from "@/Components/ListingAddress.vue";
+import { Link } from "@inertiajs/vue3";
+
+defineProps({
+    listings: Array,
+});
+</script>
+
+<style></style>
---
+++ resources/js/Pages/Listing/Show.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <ListingAddress :listing="listing" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import ListingAddress from "@/Components/ListingAddress.vue";
+
+defineProps({
+    listing: Object
+})
+</script>
+
+<style>
+
+</style>
